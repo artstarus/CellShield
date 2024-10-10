3c0ef5bc4b25aad4f6407b5d5788e789f6517af8
artstarus <artiomstarus@gmail.com>
2024-10-09 22:32:13 -0400

    Fix metadata, include twitter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -12,4 +13,34 @@ export const formatPrice = (price: number) => {
   })
   
   return formatter.format(price)
+}
+
+export function constructMetadata({
+  title = "CellShield - Custom Protective Phone Cases",
+  description = "Create personalized phone cases with custom designs, colors, and materials to express your unique style.",
+  image = "/thumbnail_shield.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{url: image}]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@artiomstarus"
+    },
+    icons,
+  }
 }
